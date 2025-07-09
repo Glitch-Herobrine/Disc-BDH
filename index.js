7badfb17829c9a39051fd379aa0939c8c8affab1
= <tunaozgur307@gmail.com>
2025-07-09 20:55:38 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,178 @@
+const readline = require('node:readline');
+const fs = require('fs');
+const chalk = require('chalk');
+const JSONbig = require('json-bigint');
+const process = require('node:process');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function ask(question){
+    rl.resume();
+    return new Promise((resolve,reject)=>{
+        rl.question(question, (answer) => {
+            rl.pause();
+            resolve(answer);
+        });
+    });
+};
+
+function readdir(path){
+    return new Promise((resolve,reject)=>{
+        fs.readdir(path,(err,files)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(files);
+            };
+        });
+    });
+};
+
+async function parseChannel(channel) {
+    let messages = [];
+    let messagesjson;
+    try{
+        messagesjson = await fs.readFileSync(`./messages/${channel}/messages.json`);
+    }catch(err){
+        return null;
+    };
+    
+    for(const msg of JSONbig.parse(messagesjson)){
+        let messageId = msg["ID"].toString();
+        if(messageId !== undefined){
+            messages.push(messageId);
+        };
+    };
+    return messages;
+};
+
+async function parseCSV(allMessages) {
+    let resultCSV = `channelid,messageid\n`;
+    for(const key of Object.keys(allMessages)){
+        let content = allMessages[key];
+        for(const idcik of content){
+            resultCSV+=`${key},${idcik}\n`;
+        };
+    };
+    return resultCSV;
+};
+
+async function  wait(ms) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+};
+
+async function dumpChannelIds() {
+    let channelsList = null;
+    do{
+        const channelsListFN = await ask(chalk.white("Enter the file that contains the list of channel ids.\n> "));
+
+        let exists = await fs.existsSync(`./${channelsListFN}`);
+
+        if(!exists){
+            console.log("The filename you provied doesn't exist.");
+            continue;
+        };
+
+        try{
+            channelsList = await fs.readFileSync(`./${channelsListFN}`,'utf-8');
+        }catch(err){
+            console.log(`Error occured while getting the channels list: ${err}`);
+            await wait(2000);
+            process.exit();
+        };
+    }while(channelsList===null);
+
+    let channels = channelsList.replaceAll(`\r`,``).split(`\n`);
+
+    if(!fs.existsSync(`./messages`)){
+        console.log(`Messages folder is not found. Make sure your messages folder is named "messages".`);
+        await wait(2000);
+        process.quit();
+    };
+
+    let channelFolders = await readdir(`./messages`);
+
+    let allMessages = {};
+
+    for(const channel of channelFolders){
+        let channelId = String(channel).replace(`c`,``);
+
+        if(channels.includes(channelId)){
+            let parsedData = await parseChannel(channel);
+            if(parsedData !== null){
+                allMessages[channelId] = parsedData;
+            };
+        };
+
+    };
+    console.log(chalk.green(`Dump complete!`));
+    const csvData = await parseCSV(allMessages);
+    try{
+        await fs.writeFileSync(`./messages.csv`,csvData);
+    }catch(err){
+        console.log(chalk.red(`An error occured: ${err}`));
+        await wait(3000);
+        process.exit(1);
+    };
+};
+
+async function dumpAll(){
+    if(!fs.existsSync(`./messages`)){
+        console.log(`Messages folder is not found. Make sure your messages folder is named "messages".`);
+        await wait(3000);
+        process.exit();
+    };
+
+    let channelFolders = await readdir(`./messages`);
+
+    let allMessages = {};
+
+    for(const channelFolder of channelFolders){
+        let channelId = String(channelFolder).replace("c","");
+        let parsedData = await parseChannel(channelFolder);
+        if(parsedData !== null){
+            allMessages[channelId] = parsedData;
+        };
+    };
+
+    const csvData = await parseCSV(allMessages);
+    try{
+        await fs.writeFileSync(`./messages.csv`,csvData);
+    }catch(err){
+        console.log(chalk.red(`An error occured: ${err}`));
+        await wait(3000);
+        process.exit(1);
+    };
+    console.log(chalk.green(`Dump complete!`))
+    await wait(1000);
+
+};
+(async () => {
+    let option = null;
+
+    do{
+        const optionans = await ask(chalk.white("Please enter the action you want to do.\n1: Dump All Messages\n2: Dump from Channel Ids\n> "));
+
+        switch(Number(optionans)){
+            case 1:
+                option = optionans;
+                dumpAll();
+                break;
+            case 2:
+                option = optionans;
+                dumpChannelIds();
+                break;
+            default:
+                option = null;
+                console.log("Please enter a valid option.");
+                break;
+        }
+    }while(option===null);
+})();
